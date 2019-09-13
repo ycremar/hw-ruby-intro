e006e4461a287631cd021009671a926b74f7d313
EC2 Default User <ec2-user@ip-172-31-32-70.us-east-2.compute.internal>
2019-09-13 17:35:07 +0000

    add solution

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,82 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  s = 0
+  arr.each{ |e|
+    s += e
+  }
+  return s
 end
+
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  m1, m2 = nil, nil
+  arr.each{ |e|
+    m2 = e if not m2 or e > m2
+    m1, m2 = m2, m1 if not m1 or m2 > m1
+  }
+  m1 = 0 if not m1
+  m2 = 0 if not m2
+  return m1+m2
 end
 
+
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length > 1
+    sarr = arr.sort
+    sarr.each_with_index { |e1, i|
+      if e1 < n
+        sarr[i+1..-1].each { |e2|
+          return true if e1 + e2 == n
+        }
+      end
+    }
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return ["Hello,", name].join(" ")
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s =~ /^[^aeiou\W]\w*/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return "0"+s =~ /^[01]*00$/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError, 'ISBN is not string' unless isbn.is_a? String
+    raise ArgumentError, 'ISBN is empty' unless isbn.length > 0
+    raise ArgumentError, 'Price is not numeric' unless price.is_a? Numeric
+    raise ArgumentError, 'Invalid price' unless price > 0
+    @isbn = isbn
+    @price = price
+  end
+  def isbn=(new_amount)
+    raise ArgumentError, 'ISBN is not string' unless isbn.is_a? String
+    raise ArgumentError, 'ISBN is empty' unless isbn.length > 0
+    @isbn = new_amount
+  end
+  def price=(new_amount)
+    raise ArgumentError, 'Price is not numeric' unless price.is_a? Numeric
+    raise ArgumentError, 'Invalid price' unless price > 0
+    @price = new_amount
+  end
+  def price
+    @price
+  end
+  def isbn
+    @isbn
+  end
+  def price_as_string
+    return "$%.2f" % [@price]
+  end
 end
